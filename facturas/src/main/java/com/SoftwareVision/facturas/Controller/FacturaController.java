25f5998d7d33c021360c420e6c9288205fa3c192
junmiron <49159206+junmiron@users.noreply.github.com>
2019-06-10 15:53:13 -0300

    added PUT update

--- facturas/src/main/java/com/SoftwareVision/facturas/Controller/FacturaController.java
+++ facturas/src/main/java/com/SoftwareVision/facturas/Controller/FacturaController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,24 +33,14 @@ public class FacturaController {
 	}
 	
 	
-	@PostMapping("/agregar")
-	@ResponseStatus(HttpStatus.OK)
-	public void create(@RequestBody Facturas facturas) {
-		
-		FR.save(facturas);
-		//facturas.setId(45);
-		//System.out.println(facturas.toString());
-		
-	}
-	
 	@GetMapping("/{id}")
-		public Facturas get(@PathVariable("id") long id) {
+	public Facturas get(@PathVariable("id") long id) {
 		
-			if(FR.existsById(id)) {
-				return FR.getOne(id);
-			}else {
-				return null;
-			}
+		if(FR.existsById(id)) {
+			return FR.getOne(id);
+		}else {
+			return null;
+		}
 		
 	}
 	
@@ -63,6 +55,31 @@ public class FacturaController {
 		}
 	}
 	
+	@PostMapping("/agregar")
+	@ResponseStatus(HttpStatus.OK)
+	public void create(@RequestBody Facturas facturas) {
+		
+		FR.save(facturas);
+		//facturas.setId(45);
+		//System.out.println(facturas.toString());
+		
+	}
+	
+	@PutMapping("/actualizar/{id}")
+	public ResponseEntity<Facturas> update(@PathVariable("id") long id, @RequestBody Facturas facturas) {
+		
+		return FR.findById(id)
+			.map(facturaUpd -> {
+				facturaUpd.setCantidad(facturas.getCantidad());
+				facturaUpd.setDescripcionDelArticulo(facturas.getDescripcionDelArticulo());
+				facturaUpd.setMarcaDelArticulo(facturas.getMarcaDelArticulo());
+				facturaUpd.setPrecioDeCompra(facturas.getPrecioDeCompra());
+				facturaUpd.setPrecioDeVenta(facturas.getPrecioDeVenta());
+				Facturas updated = FR.save(facturaUpd);
+				return ResponseEntity.ok().body(updated);
+				}).orElse(ResponseEntity.notFound().build());
+		
+	}
 	
 	
 }
